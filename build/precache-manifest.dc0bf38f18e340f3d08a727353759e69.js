bc2cd1b7f7ba5f75753fa4f33fd367c6795c7284
Fadillah Maulana <maulanafadillah.22@gmail.com>
2022-08-29 21:01:10 +0700

    commit

---
+++ build/precache-manifest.dc0bf38f18e340f3d08a727353759e69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8c9cd6cebfc347f547c4c2905b0633",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3856d4efcd59982d1f91",
+    "url": "/static/css/2.3998e652.chunk.css"
+  },
+  {
+    "revision": "0e37cf44607e7af9240a",
+    "url": "/static/css/main.82f1d36f.chunk.css"
+  },
+  {
+    "revision": "3856d4efcd59982d1f91",
+    "url": "/static/js/2.f1f34e76.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/static/js/2.f1f34e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e37cf44607e7af9240a",
+    "url": "/static/js/main.b524a39c.chunk.js"
+  },
+  {
+    "revision": "edcb260c44bf91b462e2",
+    "url": "/static/js/runtime-main.42506d08.js"
+  }
+]);
---
+++ src/containers/Home/index.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+
+import './Home.css'
+
+class HomeScreen extends Component {
+  render () {
+    return (
+      <div className='containe-style'>
+
+<div style={{ height: '100vh' }}>
+          <div className='title' id='home'>
+            HOME
+          </div>
+          <div className='paragraph'>
+          Mobile Police Patlabor (Japanese: 機動警察パトレイバー, Hepburn: Kidō Keisatsu Patoreibā), also known as Patlabor (a portmanteau of "patrol" and "labor"), is a Japanese science fiction media franchise created by Headgear, a group consisting of manga artist Masami Yūki, director Mamoru Oshii, screenwriter Kazunori Itō, mecha designer Yutaka Izubuchi, and character designer Akemi Takada. 
+          </div>
+          <div className='paragraph'>
+          The popular franchise includes a manga, a TV series, two OVA series, three feature-length movies, two light novel series, and a short film compilation, named Minipato (ミニパト) because of its super deformed (chibi) drawing style.[4] The series has been adapted into video games and licensed products from OST to toys. Patlabor is known for using mecha – designed by Yutaka Izubuchi – not just for police or military purposes, but also for industrial and municipal jobs. The series is one of the earlier examples of what is called a "media mix" in Japan, where there is no one source material: Multiple forms of media (in Patlabor's case the anime and manga) are worked on at the same time independently of each other.
+          </div>
+          <div className='paragraph'>
+          Animations from Patlabor were used extensively in the music video "Juke Joint Jezebel" by KMFDM. The manga received the 36th Shogakukan Manga Award for shōnen in 1991. 
+          </div>
+        </div>
+
+        <div style={{ height: '100vh' }}>
+          <div className='title' id='about'>
+            ABOUT
+          </div>
+          <div className='paragraph'>
+          The story takes place in what was, at the time of release, the near future of 1998–2002. Robots called "Labors" are employed in heavy construction work. The Tokyo Metropolitan Police has its own fleet of Patrol Labors ("Patlabors"; as opposed to patrol cars) to combat crimes/terrorism and deal with accidents involving Labors. The story arcs usually revolve around Tokyo Metropolitan Police Special Vehicle Section 2, Division 2. Noa Izumi is the main protagonist of the series, but all of Division 2 play roles. Hata and Kusumi are main protagonists of the third Patlabor film. The Next Generation takes place in 2013, with an entirely new cast with the exception of Shige and Buchiyama in maintenance, but the new members of SV2 have similar names and personalities to the old ones. 
+          </div>
+          <div className='paragraph'>
+          The feature films follow a separate continuity, referred to as the "movie timeline", as opposed to the "TV timeline", with the Early Days OVA following the "movie timeline", and the New Files OVA following the "TV timeline".[citation needed] In addition, the manga follows its own continuity.
+          </div>
+          <div className='paragraph'>
+          The Next Generation features episodes that directly reference the TV series, while its final episode and movie are a direct sequel to the second movie.
+          </div>
+        </div>
+
+        <div style={{ height: '100vh' }}>
+          <div className='title' id='experience'>
+            EXPERIENCE
+          </div>
+          <div className='paragraph'>
+          In the Criminal Justice System, Humongous Mecha-based offenses are considered especially heinous. In Tokyo, the dedicated officers who deal with these vicious felonies are an elite squad known as the Special Vehicles Unit. These are their stories.
+          </div>
+          <div className='paragraph'>
+          Set in the not-so-far-off future of 1998, this late-80s/early-90s anime, Mobile Police Patlabornote , is the story of police officers fighting crime with giant robots. The Special Vehicles Unit's Division 1 are a corps of competent, hard-working police who always get their man — but Patlabor isn't about them. No, it's Division 2 that gets the spotlight, that scruffy, rag-tag band of half-competent cops with a propensity towards massive property damage.
+          </div>
+          <div className='paragraph'>
+          Quite possibly the quintessential 20 Minutes into the Future giant robot anime, Patlabor is notable for treating its mecha not as insanely powerful miracle machines, but actual vehicles with clear limitations that require constant maintenance. In fact, although there's action aplenty, most of the series focuses on the daily life of the police officers who pilot the mecha, and big robot smash-ups often take up only a minute or two, if that. It is, truth be told, a Slice of Life series disguised as a Humongous Mecha show.
+          </div>
+        </div>
+
+        <div style={{ height: '100vh' }}>
+          <div className='title' id='skills'>
+            SKILLS
+          </div>
+          <div className='paragraph'>
+          Noa starts the Ark's emergency floor purging system with a device referred to as a "percussion revolver;" this is obviously incorrect, since the device does not seem to be a revolver at all, instead seemingly being a Leuchtpistole fitted with a Sturmpistole indirect fire sight. 
+          </div>
+          <div className='paragraph'>
+          Leuchtpistole - 26.65mm
+          Officers of the Special Vehicle Unit (SVU) can be seen armed with Remington 870 shotguns with high-capacity magazine tubes for use when not aboard their labors. At the climax of the anime, Noa Izume uses her Remington 870 to shoot out the onboard computer of the rampaging Type Zero labor with Kanuka trapped in the cockpit. 
+          </div>
+          <div className='paragraph'>
+          Japanese Ground Self-Defense Force soldiers at the beginning of the anime can also be seen armed with Howa Type 89 rifles. 
+          </div>
+        </div>
+
+        <div style={{ height: '100vh' }}>
+          <div className='title' id='interest'>
+            INTEREST
+          </div>
+          <div className='paragraph'>
+          In the future, advanced robotics has created heavy robots ("labors") for use in a variety of functions: construction, fire-fighting, military, and more. However, though the robots are only machines, their operators are also only human—and humans sometimes turn to crime. Since a heavy labor unit can be a dangerous weapon, the police of the future are set to fight fire with fire, using advanced patrol labor units, "patlabors". This is the story of the Second Special Vehicles Division, a motley crew of patlabor policemen and -women doing their best to fight crime and live a normal life.
+          </div>
+          <div className='paragraph'>
+          As the human race evolves, so does its technology. Engineers have successfully created robots dubbed "Labors" for mass distribution, utilized by society for a number of everyday tasks. However, there are criminals who manage to get their hands on these Labors, using them for their own nefarious means.
+          </div>
+          <div className='paragraph'>
+          To combat this new form of delinquency, police around the world begin using "Patrol Labors" or "Patlabors" to put a stop to Labor-related crimes. Rookie police officer Noa Izumi is drafted into a special Patlabor unit, getting her own mechanical suit to fight crime. Naming this machine Alphonse, Izumi works tirelessly alongside her peers to keep civilization safe from those who would use this advanced technology to harm others.
+          </div>
+        </div>
+
+     <div style={{ height: '100vh' }}>
+        <div className='title' id='awards'>
+            AWARDS
+          </div>
+          <div className='paragraph'>
+            ISBN 4091221211, July 1988
+          </div>
+          <div className='paragraph'>
+          ISBN 4091221254, December 1989
+          </div>
+          <div className='paragraph'>
+          ISBN 4091221262, March 1990
+          </div>
+          </div>
+  
+      </div>
+    );
+  }
+}
+
+HomeScreen.propTypes = {
+
+}
+
+export default HomeScreen
